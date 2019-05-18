caf021e11fab599a0ba7359742803d7c4e345e87
SzymonSzczot <38433235+SzymonSzczot@users.noreply.github.com>
2019-05-18 22:23:23 +0200

    no niby podłączony ten BT

--- Display/Debug/Src/subdir.mk
+++ Display/Debug/Src/subdir.mk
@@ -54,7 +54,7 @@ Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:/Users/Szymon/Desktop/STMePaper/SmartWatchTymRazemSerio/Inc" -I"C:/Users/Szymon/Desktop/STMePaper/SmartWatchTymRazemSerio/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/Szymon/Desktop/STMePaper/SmartWatchTymRazemSerio/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Szymon/Desktop/STMePaper/SmartWatchTymRazemSerio/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/Szymon/Desktop/STMePaper/SmartWatchTymRazemSerio/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:/Users/Szymon/Desktop/STMePaper/2019_SmartWatch/Display/Inc" -I"C:/Users/Szymon/Desktop/STMePaper/2019_SmartWatch/Display/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/Szymon/Desktop/STMePaper/2019_SmartWatch/Display/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Szymon/Desktop/STMePaper/2019_SmartWatch/Display/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/Szymon/Desktop/STMePaper/2019_SmartWatch/Display/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Display/Src/main.c
+++ Display/Src/main.c
@@ -20,9 +20,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f4xx_hal.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "stm32f4xx_hal.h"
 #include "epd1in54.h"
 #include "epdif.h"
 #include "epdpaint.h"
@@ -49,7 +50,11 @@
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart2;
-
+UART_HandleTypeDef huart3;
+uint8_t sendUART[2] = {65, 'B'};
+uint16_t sizeSendUART = 2;
+uint8_t receiveUART[1];
+uint16_t sizeReceiveUART = 1;
 /* USER CODE BEGIN PV */
 #define COLORED      0
 #define UNCOLORED    1
@@ -60,19 +65,20 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART3_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
+
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -102,7 +108,10 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
+  HAL_UART_Transmit_IT(&huart3, sendUART, sizeSendUART);
+
   EPD epd;
    if (EPD_Init(&epd, lut_full_update) != 0) {
      printf("e-Paper init failed\n");
@@ -186,6 +195,7 @@ int main(void)
    EPD_SetFrameMemory(&epd, IMAGE_DATA, 0, 0, epd.width, epd.height);
    EPD_DisplayFrame(&epd);
    EPD_DelayMs(&epd, 1000);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -329,6 +339,39 @@ static void MX_USART2_UART_Init(void)
 }
 
 /**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -340,8 +383,8 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(DC_GPIO_Port, DC_Pin, GPIO_PIN_RESET);
