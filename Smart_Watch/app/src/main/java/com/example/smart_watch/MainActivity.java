a2a6bff1dfcd68fed7ffcf17894aa5d504fa58c4
Przemko555 <44112883+Przemko555@users.noreply.github.com>
2019-05-27 20:09:25 +0200

    Poprawka Bluetooth

--- Smart_Watch/app/src/main/java/com/example/smart_watch/MainActivity.java
+++ Smart_Watch/app/src/main/java/com/example/smart_watch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.smart_watch;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,20 +15,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    Button wykryj_bluetooth, pokaz_sparowane, btn_czas;
+    Button wykryj_bluetooth, pokaz_sparowane, btn_czas, start;
     TextView tekst;
+    BluetoothAdapter myBlueotoothAdapter;
+    BluetoothSocket mySocket;
+    BluetoothDevice myDevice;
+    OutputStream out;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=(Button)findViewById(R.id.start);
         tekst=(TextView)findViewById(R.id.TEXT);
         btn_czas=(Button)findViewById(R.id.CZAS);
         wykryj_bluetooth=(Button)findViewById(R.id.wykryj);
@@ -45,19 +55,17 @@ public class MainActivity extends AppCompatActivity {
                 tekst.setText(localTime);
             }
         });
-        wykryj_bluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wykryjInne();
-            }
-        });
-        pokaz_sparowane.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pokazSparowane();
-            }
-        });
 
+       start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // try{
+                    findBluetooth();
+                    //openBluetooth();
+               //   }
+                //catch (IOException ex){}
+            }
+        });
 
     }
     private final BroadcastReceiver odbiorca= new BroadcastReceiver() {
@@ -99,4 +107,42 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    void findBluetooth(){
+        myBlueotoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBlueotoothAdapter==null){
+            tekst.setText("No bluetooth available");
+        }
+        if(!myBlueotoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth,0);
+        }
+        Set<BluetoothDevice> pairedDevices= myBlueotoothAdapter.getBondedDevices();
+        if(pairedDevices.size()>0){
+            for(BluetoothDevice device : pairedDevices){
+                if(device.getName().equals("HC-05")){
+                    myDevice = device;
+                    break;
+                }
+            }
+        }
+        tekst.setText("Znaleziono urządzenia Bluetooth");
+    }
+
+    void openBluetooth() throws IOException{
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        mySocket= myDevice.createRfcommSocketToServiceRecord(uuid);
+        mySocket.connect();
+        out = mySocket.getOutputStream();
+        tekst.setText("Bluetooth Otwarte");
+
+    }
+
+    void sendData(String msg)throws IOException{
+        msg+="\n";
+        out.write(msg.getBytes());
+        tekst.setText("Dane wysłane");
+    }
+
+
 }
